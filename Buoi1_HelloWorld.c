a939f63da698a56097244db0917621486d9a55d0
HangNT169 <HangNT169@fpt.edu.vn>
2022-11-08 01:34:49 +0700

    Buoi 1: Hello world

---
+++ Buoi1_HelloWorld.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    /**
+     * Tat cac cac dong lenh ket t huc bang dau ;
+     * printf: Dung de in bat cu thong tin mong muon
+     * \n: Dung de xuong dong
+     */
+
+    printf("hihihi\n");
+    printf("Hello world");
+}
