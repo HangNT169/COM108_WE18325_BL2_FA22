7cc883b758323414dec445be8c6f696b1cc072c7
HangNT169 <HangNT169@fpt.edu.vn>
2022-11-22 12:20:38 +0700

    Buoi 8: Loop

---
+++ Buoi 8_Loop.c
@@ -0,0 +1,119 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    // printf("1");
+    // printf("2");
+    // printf("3");
+    // printf("4");
+    // printf("5");
+    // printf("6");
+    
+    // 1.While 
+    // 1-10 
+    // bat dau in : 1 
+    // Ket thuc la : 10  
+    // k in ke tu so 11,12,.... 
+    // while(dk){
+    //     // code
+    // }
+    // int i  = 1;
+    // while(i <11){
+    //     printf("%d\n",i);
+    //     i++; // i tang len 1 don vi 
+    // }
+    // B1: In cac so tu 1 toi 50 
+    // int i = 1;
+    // while(i<51){
+    //     printf("%d\n",i);
+    //     i++;
+    // }
+    // B2: In cac so chan tu 1 toi 50 
+    // int i = 1;
+    // while(i < 51){
+    //     if(i % 2 == 0){ // Kiem tra so chan 
+    //         // in 
+    //         printf("%d\n",i);
+    //     }
+    //     i++;
+    // }
+    // B3: Tinh tong cac so tu 1 toi 10 
+    // int i = 1;
+    // int sum = 0;
+    // while(i < 11){
+    //     // tinh tong 
+    //     //  Tinh tong giong nhau 
+    //     // sum = sum +i;
+    //     // sum+=i;
+    //     // i tang len 1 don vi 
+    //     // i++;
+    //     // i = i+1;
+    //     // i+=1;
+    //     //  Rieng voi truong hop tang len 1 don vi hoac giam di 1 don vi :
+    //     // i++,i--;
+    //     // i = i +2;
+    //     // i+=2;
+    //     // i = i+3;
+    //     // i+=3;
+    // }
+    // printf("Sum = %d\n",sum);
+    // B4: Tinh tong cac so chan tu 1 toi 10 
+    // int i = 1;
+    // int sum = 0;
+    // while(i <11){
+    //     if(i % 2 ==0){
+    //         // tinh tong 
+    //         sum +=i;
+    //     }
+    //     i=i+1;
+    // }
+    // printf("Sum = %d\n",sum);
+    // B5: Dem xem tu 1 toi 50 co bao nhieu so chan 
+    // int i = 1;
+    // int count = 0;
+    // while(i <51){
+    //   if(i %2 ==0){
+    //       // dem 
+    //       count ++;
+    //   }
+    //   i++;
+    // }
+    // printf("Count = %d\n",count);
+    
+    // 2. Do ...while 
+    // - Van dung de lap
+    // do{
+    //     // code 
+    // }while(dieuKien);
+    // B1: In cac so tu 1 toi 10 
+    // int i = 1;
+    // do{
+    //   printf("%d\n",i);
+    //   i++;
+    // }while(i < 11);
+    
+    // 3. Phan biet while .... do..while
+    // giong: Vong lap 
+    // Khac 
+    // int i = 5;
+    // while(i <5 ){
+    //     printf("Nho hon 5");
+    // }
+    // int i = 5;
+    // do{
+    //     printf("Nho hon 5");
+    // }while(i <5);
+    // do..while 
+    // => Luon luon chay 1 lan neu dieu kien sai : Lam roi moi check
+    // while : 
+    // check roi moi lam => khong bao gio chay neu dieu kien sai 
+    return 0;
+}
