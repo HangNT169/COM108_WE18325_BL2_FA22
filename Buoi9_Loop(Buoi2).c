4ded35f1a3a76f461fa00830a7b030292e3b1b88
HangNT169 <HangNT169@fpt.edu.vn>
2022-11-24 11:28:22 +0700

    Tiếp về mảng for + Biến toàn cục, Biến cục bộ: Buổi 9

---
+++ Buoi9_Loop(Buoi2).c
@@ -0,0 +1,88 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    // Bai 4:
+    // int i = 1;
+    // float sum = 0;
+    // while(i < 11){
+    //     sum += 1.0/i;
+    //     i++;
+    // }
+    // printf("Sum = %f",sum);
+    // Bai 5:
+    // int menu ;
+    // do{
+    //     printf("Moi nhap menu:");
+    //     scanf("%d",&menu);
+    //     switch(menu){
+    //         case 1:{
+    //             // lam chuc nang 1 
+    //             break;
+    //         }
+    //         //.. tuong tu cac case case con lai 
+    //         case 5:{
+    //             break;
+    //         }
+    //         default:{
+    //             printf("Chuc nang khong ton tai");
+    //             break;
+    //         }
+    //     }
+    // }while(menu !=5);
+    // For 
+    // for(int i = 0;i<11;i++){
+    //     printf("%d\n",i);
+    // }
+    // B1: Liet ke cac so tu 0 toi 20.
+    // B2: Tinh tong cac so chan tu 0 toi 20 
+    // int sum = 0;
+    // for(int i=0;i<21;i++){
+    //     if(i%2==0){
+    //         sum+=i;
+    //     }
+    // }
+    // printf("Sum = %d\n",sum);
+    // // B3: Dem cac so le tu 0 toi 20 
+    // int count = 0;
+    // for(int i = 0;i<21;i++){
+    //     if(i % 2 !=0){
+    //         count++;
+    //     }
+    // }
+    // printf("Count = %d\n",count);
+    // B4: Tinh tich cac so chia het cho 6 tu 1 toi 100 
+    // long tich = 1;
+    // for(int i = 1;i<101;i++){
+    //     if(i % 6 == 0){
+    //         tich *= i;
+    //     }
+    // }
+    // printf("Tich = %ld\n",tich);
+    // B5: Nhap vao so n. Liet ke cac so tu 0 toi n
+    // int n ;
+    // printf("Nhap vao so n:");
+    // scanf("%d",&n);
+    // for(int i = 0 ;i <= n ;i++){
+    //     printf("%d\n",i);
+    // }
+    // Co 2 loai bien :
+    // Bien toan cuc :
+    // Bien cuc bo : 
+    int i = 0;
+    for(;i<10;i++){
+        printf("%d\n",i);
+    }
+    printf("Sau vong lap :\n");
+    printf("%d\n",i);
+
+    return 0;
+}
